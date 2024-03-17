20e2219d533363ed0e12798cdb7c8fcfabbdc434
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-18 03:06:48 +0900

    first commit.

---
+++ FixGachaListApp/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = args.FirstOrDefault();
+if (!File.Exists(input))
+{
+    Console.WriteLine("not found.");
+    Environment.Exit(1);
+}
+
+File.WriteAllLines(input, 
+    File.ReadAllLines(input)
+        .Select(e => e.Split(','))
+        .Select(s=> (s[0], s[1], s[3]))
+        .Select(t => $"{t.Item1},{t.Item2},{t.Item3}"));
